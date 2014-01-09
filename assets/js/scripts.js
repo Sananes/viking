38dde4bd68b5b1dca48312e4060be52652ca9544
Aaron Sananes <aasananes@gmail.com>
2014-01-09 16:12:04 +0000

    Added new animation, and infinite scroll :D

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,6 +15,293 @@
 
  */
 
+(function ($) {
+    "use strict";
+
+    function get_feature_image() {
+        var articles_links = $(".post-title a"),
+            featured_parts = $(".featured"),
+            list_links = [];
+
+        /* -- Feature Images and Video per Post -- */
+        if (articles_links.length > 0) {
+            
+//            if ( Modernizr.localstorage ) {
+//                localStorage.clear();
+//            }
+            
+            articles_links.each(function (index) {
+                //Process script                 
+                list_links.push(articles_links[index].href);
+                /* -- Next and Prev Post -- */
+                if ( Modernizr.localstorage ) {
+                localStorage.setItem(articles_links[index].href,
+                    JSON.stringify({"prev": (articles_links[index - 1] != null ? articles_links[index - 1].href : "none"),
+                                    "next": (articles_links[index + 1] != null ? articles_links[index + 1].href : "none")}));
+                }
+            });
+        }
+
+        if (list_links.length > 0) {
+            list_links.forEach(function (element, index, array) {
+                $.get(list_links[index], function (data) {
+                    var html = $(data),
+                        article = html.find("article"),
+                        img = article.find("img:first"),
+                        video = article.find("iframe:first"),
+                        featured = featured_parts[index];
+
+                    if (img.length > 0 && video.length > 0) {
+                        $(featured).html(img[0]);
+                    } else {
+                        if (img.length > 0) {
+                            $(featured).html(img[0]);
+                        } else if (video.length > 0) {
+                            $(featured).html(video[0].outerHTML);
+                        }
+                    }
+                    $(".post").fitVids();
+                });
+            });
+        }
+    }
+
+    
+
+    //Function check menu opened
+    function move_scroll_icon() {
+        if (menu_open()) {
+            $(".scrollup").removeClass("scroll-right");
+            $(".scrollup").addClass("scroll-left");
+        } else {
+            $(".scrollup").removeClass("scroll-left");
+            $(".scrollup").addClass("scroll-right");
+        }
+    }
+
+    //Function generate background
+
+    function getbg() {
+        var d = new Date(),
+            today = parseInt(d.getDate(), 10),
+            bg = "1.png";
+
+        if (today <= 5) {
+            bg = "6.png";
+        } else if (today <= 10) {
+            bg = "5.png";
+        } else if (today <= 15) {
+            bg = "4.png";
+        } else if (today <= 20) {
+            bg = "3.png";
+        } else if (today <= 25) {
+            bg = "2.png";
+        } else {
+            bg = "1.png";
+        }
+        return bg;
+    }
+
+    function menu_open() {
+        return ($("#menu").css("right") == "0px");
+    }
+
+    function detect_browser() {
+        var browser = {
+            chrome: false,
+            mozilla: false,
+            opera: false,
+            msie: false,
+            safari: false
+        },
+            sBrowser, sUsrAg = navigator.userAgent;
+
+        if (sUsrAg.indexOf("Chrome") > -1) {
+            browser.chrome = true;
+        } else if (sUsrAg.indexOf("Safari") > -1) {
+            browser.safari = true;
+        } else if (sUsrAg.indexOf("Opera") > -1) {
+            browser.opera = true;
+        } else if (sUsrAg.indexOf("Firefox") > -1) {
+            browser.mozilla = true;
+        } else if (sUsrAg.indexOf("MSIE") > -1 || sUsrAg.indexOf("Trident") > -1) {
+            browser.msie = true;
+        }
+        return browser;
+    }
+
+    function prev_next() {
+        if ( Modernizr.localstorage ) {
+            if (localStorage.getItem(window.location.href) != null && window.location.href != (window.location.origin + "/")) {
+                var relations = localStorage.getItem(window.location.href);
+                return JSON.parse(relations);
+            }
+        } else {
+            return false;
+        }
+    }
+    
+    function enable_infinite() {
+        if ( Modernizr.localstorage ) {
+            if (localStorage.getItem("enable_infinite_scrolling") == null) {
+                localStorage.setItem("enable_infinite_scrolling", "true");
+                return true;
+            } else if (localStorage.getItem("enable_infinite_scrolling") == "true") {
+                return true;
+            } else if (localStorage.getItem("enable_infinite_scrolling") == "false") {
+                $("#infinite_enable").removeProp("checked");
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+        
+
+    $(document).ready(function () {
+        
+        /* -- Detect IE -- */
+        var browser = detect_browser(),
+            logo_text = $("#flash").text() + " " + $("#light").text(),
+            is_many_page = $(".pagination a.older-posts").length,
+            is_infinite = enable_infinite(),
+            rel_links = prev_next();
+        
+        /* -- Fallback text logo for IE -- */
+        if (browser.msie == true) {
+            $("#box").html(logo_text);
+        }
+        
+        /* -- Hide pagination for infinite scrolling  -- */        
+        if (is_infinite || is_many_page == 0)
+        {
+            $(".pagination").css("display", "none" );
+        } else {
+            $(".pagination").css("display", "block");
+        }
+        
+        /* -- Catch checkbox event -- */
+        $("input#infinite_enable").change( function() {
+            localStorage.setItem("enable_infinite_scrolling", $(this).is(":checked"));
+            location.reload();
+        });
+        
+        /* -- Code Highlight -- */
+        $("pre").addClass("prettyprint");
+
+
+
+        /* -- Handle menu event -- */
+        $(".toggle-icon").click(function () {
+            if (menu_open()) {
+                $("#menu").css("right", "-280px");
+                $(".scrollup").removeClass("scroll-left");
+                $(".scrollup").addClass("scroll-right");
+            } else {
+                $("#menu").css("right", "0");
+                $(".scrollup").removeClass("scroll-right");
+                $(".scrollup").addClass("scroll-left");
+            }
+        });
+
+        $(document).click(function (event) {
+            if ($(event.target).parents().index($("#menu")) == -1) {
+                if (menu_open()) {
+                    $("#menu").css("right", "-280px");
+                    $(".scrollup").removeClass("scroll-left");
+                    $(".scrollup").addClass("scroll-right");
+                }
+            }
+        });
+
+        /* -- Change Background -- */
+        $("body").css("background", "url(\"/assets/imgs/bg/" + getbg() + "\")");
+//        
+//        if (Modernizr.localstorage)
+//        {
+//            $(".pagination").css("display", "none");
+//        } else {
+//            //Using jQuery Cookie
+//        }
+//        
+        $(window).scroll(function () {
+            /* -- Scroll to Top -- */
+            if ($(this).scrollTop() > 100) {
+                //Display it
+                $(".scrollup").fadeIn();
+            } else {
+                $(".scrollup").fadeOut();
+            }
+            
+            if (localStorage.getItem("enable_infinite_scrolling") == "true") {
+                /* -- Endless Scrolling -- */
+                var next_page = $(".pagination a.older-posts");
+                if (next_page.length > 0) {
+                    next_page = next_page.attr("href");
+                    if ($(window).scrollTop() == ($(document).height() - $(window).height())) {
+                        $(".endless").show();
+                        $.ajax({
+                            url: next_page,
+                            success: function (html) {
+                                if (html) {
+                                    var content = $(html),
+                                        posts = content.find("article.post"),
+                                        pagination = content.find(".pagination"),
+                                        endless = content.find(".endless");
+    
+                               
+                                    $(".pagination").remove();
+    
+                                    $(".endless").remove();
+                                    posts.each(function (index) {
+                                        $(posts[index]).appendTo("#main").addClass('animated fadeIn');
+                                    });
+    
+                                    //Callback when create new article
+                                    //get_feature_image();
+                                    //add_weather_emo();
+    
+                                    $("#main").append(endless);
+                                    $("#main").append(pagination);
+                                    $(".pagination").css("display", "none");
+                                    $(".endless").hide(300);
+                                }
+                            }
+                        });
+                    } else {
+                        if ($(".pagination").css("display") == "block") {
+                            $(".no-more").removeClass("animated shake");
+                            $(".no-more").addClass("animated shake");
+                        }
+        
+                    }
+                } else {
+                    $(".pagination").css("display", "block");
+                    $(".pagination").html("<span class=\"button no-more\">''-_-</span>");
+                    $(".no-more").removeClass("animated shake");
+                    $(".no-more").addClass("animated shake");
+                }
+            }
+        });
+
+        //get_feature_image();
+                    
+        if (rel_links != null && Modernizr.localstorage && rel_links != false) {
+            $(document).keydown(function (event) {
+                if (event.which == 37 && rel_links.prev != "none") {
+                    event.preventDefault();
+                    window.location.href = rel_links.prev;
+                }
+                if (event.which == 39 && rel_links.next != "none") {
+                    event.preventDefault();
+                    window.location.href = rel_links.next;
+                }
+            });
+        }
+    });
+
+}(jQuery));
+
 // IE8 ployfill for GetComputed Style (for Responsive Script below)
 if (!window.getComputedStyle) {
     window.getComputedStyle = function(el, pseudo) {
@@ -120,6 +407,8 @@ jQuery(document).ready(function($) {
     //   }
     // });
 
+
+
     $('.back-to-top').click(function () {
       $("html, body").animate({
           scrollTop: 0
@@ -127,8 +416,10 @@ jQuery(document).ready(function($) {
       return false;
     });
 
-}); /* end of as page load scripts */
 
+
+}); /* end of as page load scripts */
+    
 
 /*! A fix for the iOS orientationchange zoom bug.
  Script by @scottjehl, rebound by @wilto.
